f35759cc6a771d1ced494db5f8de0894ec690fd8
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:46:23 +0530

    Create 1667-FindKthBitInNthBinaryString

---
+++ 1667-FindKthBitInNthBinaryString/1667-FindKthBitInNthBinaryString.java
@@ -0,0 +1,15 @@
+// Last updated: 4/11/2026, 12:46:24 PM
+class Solution 
+{
+  public char findKthBit(int n, int k) 
+  {
+    if (n == 1)
+      return '0';
+    final int midIndex = (int) Math.pow(2, n - 1);
+    if (k == midIndex)
+      return '1';
+    if (k < midIndex)
+      return findKthBit(n - 1, k);
+    return findKthBit(n - 1, midIndex * 2 - k) == '0' ? '1' : '0';
+  }
+}
