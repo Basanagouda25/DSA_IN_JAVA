19c24de532054638def904b00c9ddcb2e26453aa
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:46:34 +0530

    Create 1557-CheckIfAStringContainsAllBinaryCodesOfSizeK

---
+++ 1557-CheckIfAStringContainsAllBinaryCodesOfSizeK/1557-CheckIfAStringContainsAllBinaryCodesOfSizeK.java
@@ -0,0 +1,21 @@
+// Last updated: 4/11/2026, 12:46:35 PM
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        int req = 1 << k;
+        boolean[] seen = new boolean[req];
+        int mask = req - 1;
+        int hash = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            hash = ((hash << 1) & mask) | (s.charAt(i) & 1);
+
+            if (i >= k - 1 && !seen[hash]) {
+                seen[hash] = true;
+                req--;
+                if (req == 0) return true;
+            }
+        }
+
+        return false;
+    }
+}
