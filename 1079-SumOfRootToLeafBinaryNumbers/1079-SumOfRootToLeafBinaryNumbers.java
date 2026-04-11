ea5209e37af04978c4c14a6a2ae2571620a0e3e5
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:47:13 +0530

    Create 1079-SumOfRootToLeafBinaryNumbers

---
+++ 1079-SumOfRootToLeafBinaryNumbers/1079-SumOfRootToLeafBinaryNumbers.java
@@ -0,0 +1,36 @@
+// Last updated: 4/11/2026, 12:47:14 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode node, int current) {
+        if (node == null) {
+            return 0;
+        }
+        
+        current = current * 2 + node.val;
+        
+        if (node.left == null && node.right == null) {
+            return current;
+        }
+        
+        return dfs(node.left, current) + dfs(node.right, current);
+    }
+}
