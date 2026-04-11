69cb9b07b3d97aebd4fdc28893b303ca3f42c01d
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:50:23 +0530

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.java
@@ -0,0 +1,18 @@
+// Last updated: 4/11/2026, 12:50:23 PM
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+     int l=0,h=n;
+     while(l <= h){
+        int mid = l + (h-l)/2;
+        if(isBadVersion(mid)){
+            h = mid - 1;
+        }else{
+            l = mid+1;
+        }
+     }
+     return l;
+    }
+}
