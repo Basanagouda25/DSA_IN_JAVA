61d6d8dba03d463e0a670fdf1994a0b89b70a5cd
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:46:49 +0530

    Create 1444-NumberOfStepsToReduceANumberToZero

---
+++ 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,15 @@
+// Last updated: 4/11/2026, 12:46:51 PM
+class Solution {
+    public int numberOfSteps(int num) {
+        return fun(num,0);
+    }
+    public int fun(int num,int steps){
+        if(num == 0){
+            return steps;
+        }
+        if(num%2 == 0){
+            return fun(num/2,steps+1);
+        }
+        return fun(num-1,steps+1);
+    }
+}
