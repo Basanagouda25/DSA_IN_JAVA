b9baac985155cb42c759d7014dd1cc28d0114851
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:51:48 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.java
@@ -0,0 +1,13 @@
+// Last updated: 4/11/2026, 12:51:49 PM
+class Solution {
+        public int divide(int A, int B) {
+        if (A == 1 << 31 && B == -1) return (1 << 31) - 1;
+        int a = Math.abs(A), b = Math.abs(B), res = 0;
+        for (int x = 31; x >= 0; x--)
+            if ((a >>> x) - b >= 0) {
+                res += 1 << x;
+                a -= b << x;
+            }
+        return (A > 0) == (B > 0) ? res : -res;
+    }
+}
