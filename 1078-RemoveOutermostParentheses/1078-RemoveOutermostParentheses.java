be64edd0207082236ade40b1b2c5da293825bdda
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:47:14 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.java
@@ -0,0 +1,19 @@
+// Last updated: 4/11/2026, 12:47:15 PM
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count =0;
+        StringBuilder str = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count != 0){
+                str.append(s.charAt(i));
+            }
+            if(s.charAt(i)== '('){
+                count++;
+            }
+        }
+        return str.toString();
+    }
+}
