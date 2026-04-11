436b88a089bf297a56ad65c81adcb58a781782d9
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:49:55 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,17 @@
+// Last updated: 4/11/2026, 12:49:56 PM
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count=0;
+        int i=0,j=0;
+        while(i < g.length && j < s.length){
+            if(s[j] >= g[i]){
+                count++;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+}
