59d658eebc74ec14f7b983bd6f4c78d8e6bfb577
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:51:29 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,17 @@
+// Last updated: 4/11/2026, 12:51:31 PM
+class Solution {
+    public int climbStairs(int n) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        return climbStairs(n,map);
+    }
+
+    int climbStairs(int n,HashMap<Integer,Integer>map){
+        if(n == 0 || n==1){
+            return 1;
+        }
+        if(!map.containsKey(n)){
+            map.put(n,climbStairs(n-1,map) + climbStairs(n-2,map));
+        }
+        return map.get(n);
+    }
+}
