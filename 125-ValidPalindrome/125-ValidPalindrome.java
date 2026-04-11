ae78c49c015014d6052f8ace5adf00bf73446a12
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:51:07 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,17 @@
+// Last updated: 4/11/2026, 12:51:09 PM
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
