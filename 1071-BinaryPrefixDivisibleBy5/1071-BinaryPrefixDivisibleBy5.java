06c613cbeddc14c41ec297d9f30bcc8e6ea09a09
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:47:16 +0530

    Create 1071-BinaryPrefixDivisibleBy5

---
+++ 1071-BinaryPrefixDivisibleBy5/1071-BinaryPrefixDivisibleBy5.java
@@ -0,0 +1,16 @@
+// Last updated: 4/11/2026, 12:47:17 PM
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> list = new ArrayList<>();
+        int num = 0;
+        for(int i=0; i<nums.length; i++){
+            num = (num * 2 + nums[i]) % 5;
+            if(num == 0){
+                list.add(true);
+            }else{
+                list.add(false);
+            }
+        }
+        return list;
+    }
+}
