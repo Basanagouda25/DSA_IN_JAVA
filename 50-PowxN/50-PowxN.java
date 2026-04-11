90ea24d411f7ecb1ba40f1f3bbd49baf8c8ead90
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:51:37 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,9 @@
+// Last updated: 4/11/2026, 12:51:38 PM
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        return Math.pow(x,n);
+    }
+}
