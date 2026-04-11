5ca4b22b9a011be4ef67e75e659f5810663c294f
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:46:14 +0530

    Create 1755-DefuseTheBomb

---
+++ 1755-DefuseTheBomb/1755-DefuseTheBomb.java
@@ -0,0 +1,24 @@
+// Last updated: 4/11/2026, 12:46:15 PM
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] res = new int[n];
+        for(int i=0; i<n; i++){
+            int sum=0;
+            if(k > 0){
+                for(int j=1; j<=k; j++){
+                    int next = (i+j) % n;
+                    sum +=code[next];
+                }
+            }
+            else if(k < 0){
+                for(int j=1; j<= -k; j++){
+                    int next = (i-j+n)%n;
+                    sum +=code[next];
+                }
+            }
+            res[i] = sum;
+        }
+        return res;
+    }
+}
