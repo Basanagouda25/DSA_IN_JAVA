ba1769bea19767bc9eeada375b71d71ff85aff6b
Basanagouda <basanagoudad25@gmail.com>
2026-04-11 12:50:15 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.java
@@ -0,0 +1,28 @@
+// Last updated: 4/11/2026, 12:50:16 PM
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int n = nums1.length;
+        int m = nums2.length;
+        int i = 0, j = 0;
+        List<Integer> list = new ArrayList<>();
+        while (i < n && j < m) {
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                while (i < n - 1 && nums1[i] == nums1[i + 1]) {
+                    i++;
+                }
+                while (j < m - 1 && nums2[j] == nums2[j + 1]) {
+                    j++;
+                }
+                i++;j++;
+            }else if(nums1[i] < nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
