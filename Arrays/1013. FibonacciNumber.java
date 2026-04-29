d6c6f04a47f00994bfbe9f4bf6f48abac8032cb2
Basanagouda <basanagoudad25@gmail.com>
2026-04-29 07:34:37 +0530

    Update 1013. FibonacciNumber.java

--- Arrays/1013. FibonacciNumber.java
+++ Arrays/1013. FibonacciNumber.java
@@ -1,4 +1,4 @@
-// Last updated: 4/11/2026, 12:47:35 PM
+// Recurssive Solution
 class Solution {
     public int fib(int n) {
         if(n==0){
@@ -7,5 +7,26 @@ class Solution {
             return 1;
         }
         return fib(n-1)+fib(n-2);
+    }
+}
+
+// Memoization solution
+class Solution {
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        if(n <= 1){
+            return n;
+        }
+        return solve(n, dp);
+    }
+    public int solve(int n, int[] dp){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = solve(n - 1, dp) + solve(n - 2,dp);
+        return dp[n];
     }
 }
